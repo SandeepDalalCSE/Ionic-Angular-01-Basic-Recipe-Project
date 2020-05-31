b0bd9a83aac9f756d0264e179862ebe675d8d629
Sandeep Dalal <sandeepdalal1995@gmail.com>
2020-06-01 01:19:15 +0530

    Rendering details for single recipe and assign path using recipeId to it in recipes.page.html

--- src/app/recipes/recipe-detail/recipe-detail.page.ts
+++ src/app/recipes/recipe-detail/recipe-detail.page.ts
@@ -1,4 +1,7 @@
+import { RecipesService } from './../recipes.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeDetailPage implements OnInit {
 
-  constructor() { }
+  loadedRecipe: Recipe;
+
+  constructor(private activatedRoute: ActivatedRoute, private recipeService: RecipesService) { }
 
   ngOnInit() {
+    this.activatedRoute.paramMap.subscribe(paramMap => {
+      if (!paramMap.has('recipeId')) {
+        // redirect
+        return;
+      }
+      const recipeId = paramMap.get('recipeId'); // getting id from the paramMap
+      this.loadedRecipe = this.recipeService.getRecipe(recipeId); // loads a single recipe and stored it into loadedRecipe property
+    });
   }
 
 }
