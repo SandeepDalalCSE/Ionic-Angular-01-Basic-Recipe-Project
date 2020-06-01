b4000ae48196f2905b7914b0b4b5b36273ea35d6
Sandeep Dalal <sandeepdalal1995@gmail.com>
2020-06-01 07:58:47 +0530

    Ionic Page Caching & Extra Lifecycle Hooks Implementing

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,19 +1,39 @@
 import { RecipesService } from './recipes.service';
 import { Recipe } from './recipe.model';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.page.html',
   styleUrls: ['./recipes.page.scss'],
 })
-export class RecipesPage implements OnInit {
+export class RecipesPage implements OnInit, OnDestroy {
   recipes: Recipe[];
 
   constructor(private recipeService: RecipesService) { }
 
   ngOnInit() {
+    console.log("ngOnInit");
+    // this.recipes = this.recipeService.getAllRecipe();
+  }
+
+  ionViewWillEnter() {
+    console.log("ionViewWillEnter");
     this.recipes = this.recipeService.getAllRecipe();
+
+  }
+  ionViewDidEnter() {
+    console.log("ionViewDidEnter");
+  }
+  ionViewWillLeave() {
+    console.log("ionViewWillLeave");
+  }
+  ionViewDidLeave() {
+    console.log("ionViewDidLeave");
+  }
+
+  ngOnDestroy() {
+    console.log("ngOnDestroy");
   }
 
 }
